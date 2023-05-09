bbccdef381ac1fc2ccf26a321c13d8e77d870edd
John Chong <johnchong.99.17@gmail.com>
2023-05-09 15:35:44 -0500

    work on step2

--- step2.js
+++ step2.js
@@ -3,6 +3,10 @@ const fsP = require("fs/promises");
 const cmnPath = process.argv[2];
 const axios = require("axios");
 
+
+/** takes in input and reads file if valid path,
+ *  else returns error message
+ */
 async function cat(path) {
   try {
     const holder = await fsP.readFile(path, "utf8");
@@ -12,21 +16,29 @@ async function cat(path) {
   }
 }
 
+/** takes in url and shows content if valid url,
+ *  else returns error message
+ */
 async function webCat(URL) {
-  const result = await axios.get(URL);
-  console.log("rsult is", result);
-}
-
-function runUrlOrTxt(input) {
   try {
-    if (new URL(input)) {
-      webCat(input);
-    } else {
-      cat(input);
-    }
+    const result = await axios.get(URL);
+    console.log("result", result.status);
+    // console.log("result is", result);
   } catch (error) {
-    console.log("ERROR", error);
+    console.log("Error: Request failed with status code 404");
   }
 }
+
+/** conductor function that calls either cat or webCat depending
+ *  on command line input.
+ */
+function runUrlOrTxt(input) {
+  if (input.startsWith('http')) {
+    webCat(input);
+  } else {
+    cat(input);
+  }
+}
+
 
 runUrlOrTxt(cmnPath);
